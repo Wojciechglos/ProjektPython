20241fad5c53564f71547c87f767934cc3d2b3e0
Wojciech Głos <wglos@edu.cdv>
2024-06-08 16:08:56 +0200

    poprawa wykresu o wartości min max

--- Project/wykres.py
+++ Project/wykres.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import numpy as np
 
-
 def wykres_danych(data):
     # Konwersja dat z formatu tekstowego na obiekty datetime i umieszczenie ich w liście znaczników_czasu
     znaczniki_czasu = [datetime.strptime(wiersz[4], '%Y-%m-%d %H:%M:%S') for wiersz in data]
@@ -33,6 +32,14 @@ def wykres_danych(data):
         # Dodanie serii danych do wykresu dla danego parametru
         linia, = plt.plot(znaczniki_czasu_parametru, wartosci_parametru, label=nazwa_parametru)
 
+        # Dodanie kropki dla wartości minimalnej na wykresie
+        wartosc_minimalna = np.min(wartosci_parametru)
+        plt.scatter(znaczniki_czasu_parametru[wartosci_parametru.index(wartosc_minimalna)], wartosc_minimalna, color='green', zorder=5)
+
+        # Dodanie kropki dla wartości maksymalnej na wykresie
+        wartosc_maksymalna = np.max(wartosci_parametru)
+        plt.scatter(znaczniki_czasu_parametru[wartosci_parametru.index(wartosc_maksymalna)], wartosc_maksymalna, color='red', zorder=5)
+
         # Dodanie linii trendu
         if wartosci_parametru:  # Sprawdź, czy wartosci_parametru nie jest puste
             wspolczynniki_wielomianu = np.polyfit(range(len(wartosci_parametru)), wartosci_parametru, 1)
@@ -40,12 +47,10 @@ def wykres_danych(data):
             plt.plot(znaczniki_czasu_parametru, linia_trendu, linestyle='--', color='orange')
 
         # Analiza danych
-        wartosc_minimalna = np.min(wartosci_parametru)
-        wartosc_maksymalna = np.max(wartosci_parametru)
         wartosc_srednia = np.mean(wartosci_parametru)
         trend = "wzrost" if wspolczynniki_wielomianu[0] < 0 else "spadek"
 
-        # Dodanie danych w legendzie
+        # Dodanie danych w legendzie z odpowiednimi kolorami
         plt.legend(handles=[linia], labels=[f'{nazwa_parametru} (min: {wartosc_minimalna:.1f}, śr: {wartosc_srednia:.1f}, max: {wartosc_maksymalna:.1f})'], loc='upper left')
 
     # Dodanie etykiet osi x i y
